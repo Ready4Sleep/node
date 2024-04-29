8cf781082ff4d32d77c45190940025056043e249
Fisherman August <fshrmn.august@gmail.com>
2024-04-29 16:11:37 +0300

    added page counters

--- 1_tasks/homework.js
+++ 1_tasks/homework.js
@@ -1,14 +1,17 @@
 const http = require("http");
- 
+let counterAboutPage = 0;
+let counterHomePage = 0;
 const server =  http.createServer((request, response) => {
     response.setHeader("Content-Type", "text/html; charset=utf-8;");
-     
+    
     if(request.url === "/home" || request.url === "/"){
         response.statusCode = 200;
         response.write(`
             <h2>Home</h2>
             <a href="/about">About page</a>
         `);
+        counterHomePage += 1;
+        console.log('home counter: ', counterHomePage);
     }
     else if(request.url == "/about"){
         response.statusCode = 200;
@@ -16,6 +19,8 @@ const server =  http.createServer((request, response) => {
             <h2>About</h2>
             <a href="/">Home page</a>
         `);
+        counterAboutPage += 1;
+        console.log('about counter: ',counterAboutPage);
     }
     else{
         response.statusCode = 404;
