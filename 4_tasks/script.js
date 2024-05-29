5ed0090088d52a589879921353ba2810ca8642f0
Fisherman August <fshrmn.august@gmail.com>
2024-05-29 23:23:20 +0300

    added REST API with const var data

---
+++ 4_tasks/script.js
@@ -0,0 +1,82 @@
+
+const express = require('express');
+const joi = require('joi');
+const app = express();
+const port = 3000;
+let users = [];
+let uniqID = 0;
+
+const userSchema = joi.object({
+    firstName: joi.string().min(1).required(),
+    secondName: joi.string().min(1).required(),
+    age: joi.number().min(0).max(150).required(),
+    city: joi.string().min(1)
+})
+
+app.use(express.json())
+
+app.get('/users', (req, res) => {
+    res.send({users});
+  })
+
+app.post('/users', (req, res) => {
+    uniqID += 1
+    users.push({
+        id: uniqID,
+        ...req.body
+    })
+    res.send({id: uniqID})
+})
+
+app.put('/users/:id', (req,res) => {
+
+    const result = userSchema.validate(req.body)
+    if (result.error) {
+        return res.status(404).send({error: result.error.details})
+    }
+    const userID = +req.params.id;
+    const user = users.find(user => user.id === userID);
+
+    if (user){
+        const {firstName, secondName, age, city } = req.body;
+        user.firstName = firstName;
+        user.secondName = secondName;
+        user.age = age;
+        user.city = city;
+        res.send({ user });
+    } else {
+        res.status(404);
+        res.send({user: null});
+    }
+})
+
+app.get('/users/:id', (req, res) => {
+
+
+    const userID = +req.params.id;
+    const user = users.find(user => user.id === userID);
+
+    if (user){
+        res.send({ user });
+    } else {
+        res.status(404);
+        res.send({user: null});
+    }
+  })
+
+app.delete('/users/:id', (req,res) => {
+    const userID = +req.params.id;
+    const user = users.find(user => user.id === userID);
+    if (user){
+        const userIndex = users.indexOf(user);
+        users.splice(userIndex, 1);
+        res.send({ user });
+    } else {
+        res.status(404);
+        res.send({user: null});
+    }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
